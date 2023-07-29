e28da247e5a8de964b502d5aaf791e19b64eba0c
Akito <the@akito.ooo>
2023-07-30 01:20:27 +0200

    Extend Configuration Model

--- src/zoominvitr/model/configuration.nim
+++ src/zoominvitr/model/configuration.nim
@@ -8,12 +8,25 @@ import
   ]
 
 type
+  ConfigAuthentication* = object
+    mail*: string   ## E-Mail address used to log into this particular Zoom account.
+    userID*: string ## User ID provided by Zoom.
+  ConfigMailSender* = object
+    mail*: string       ## E-Mail address used to SEND invitations to sendees in the same context.
+    serverSMTP*: string ## SMTP server.
+    portSMTP*: int      ## SMTP server.
+    user*: string       ## Login username.
+    password*: string   ## Login username.
   ConfigMailAddressList* = object
     topic*: string ## A keyword, which will be searched as a\
                    ## substring in the Meeting topics,\
                    ## to able to associate the E-Mail addresses\
                    ## from this object to the Meetings matching this keyword.
     mails*: seq[string] ## E-Mail Addresses associated with this object's Meeting topic.
+  ConfigContext* = object
+    authentication*: ConfigAuthentication
+    mailSender*: ConfigMailSender
+    mailAddressList*: ConfigMailAddressList
   ConfigMaster* = object
     version*: int
-    mailAddressList*: ConfigMailAddressList
+    contexts*: seq[ConfigContext]
