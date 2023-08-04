b18c150a2b64c9c90d39a452d02092de1aa21c2e
Akito <the@akito.ooo>
2023-08-04 22:35:09 +0200

    Improve Redis Layout

--- src/zoominvitr/database.nim
+++ src/zoominvitr/database.nim
@@ -35,18 +35,18 @@ import
 let
   # redis = newRedisConn("redis")
   redis = newRedisConn()
+  redisTestList = [
+    ("MULTI", @[]),
+    ("DEL", @["test1"]),
+    ("RPUSH", @["test1", "item1"]),
+    ("RPUSH", @["test1", "item1"]),
+    ("RPUSH", @["test1", "item1"]),
+    ("EXEC", @[])
+  ]
 
-redis.send("MULTI")
-redis.send("DEL", "test1")
-redis.send("RPUSH", "test1", "item1")
-redis.send("RPUSH", "test1", "item2")
-redis.send("RPUSH", "test1", "item3")
-redis.send("EXEC")
-# echo redis.receive().to(seq[string])
-echo redis.receive()
-echo redis.receive()
-echo redis.receive()
-echo redis.receive()
-echo redis.receive()
-echo redis.receive()#.to(seq[string])
-echo redis.command("LRANGE", "test1", "0", "-1")#.to(seq[string])
+redis.send(redisTestList)
+
+for e in redisTestList:
+  echo redis.receive()
+
+echo redis.command("LRANGE", "test1", "0", "-1")
