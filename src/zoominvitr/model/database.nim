9863f2a6228b414bf8c126b65d992b59aa7a0d3e
Akito <the@akito.ooo>
2023-08-06 04:24:10 +0200

    Implement Database Model

--- src/zoominvitr/model/database.nim
+++ src/zoominvitr/model/database.nim
@@ -1,3 +1,16 @@
+import
+  configuration,
+  pkg/[
+    timestamp
+  ]
+
 type
-  ConfigDatabase* = object
-    test*: string
+  DatabaseNotified* = object
+    keywordSignature*: string ## Hash-like thing of `patternKeywordsYes` & `patternKeywordsNo`.
+    timestamp*: string        ## Timestamp Library
+
+proc createDatabaseNotified*(patternKeywordsYes, patternKeywordsNo: seq[ConfigZoomPatternKeyword]): DatabaseNotified =
+  DatabaseNotified(
+    keywordSignature: $patternKeywordsYes & $patternKeywordsYes, #TODO Use some smarter hash-alike.
+    timestamp: $initTimestamp()
+  )
