bba7789fef09f75854b3dd57e735177e1c5b6c68
Akito <the@akito.ooo>
2023-08-14 18:47:50 +0200

    Overhaul Database Functionality

--- src/zoominvitr/model/database.nim
+++ src/zoominvitr/model/database.nim
@@ -1,10 +1,17 @@
 import
+  ../meta,
   configuration,
+  std/[
+    logging
+  ],
   pkg/[
     timestamp
   ]
 
+let logger = getLogger("model/database")
+
 type
+  DatabaseDeserialisationDefect* = object
   DatabaseNotified* = object
     keywordSignature*: string ## Hash-like thing of `patternKeywordsYes` & `patternKeywordsNo`.
     timestamp*: string        ## Timestamp Library
@@ -12,13 +19,13 @@ type
 proc createDatabaseNotified*(patternKeywordsYes, patternKeywordsNo: seq[ConfigZoomPatternKeyword]): DatabaseNotified =
   DatabaseNotified(
     keywordSignature: $patternKeywordsYes & $patternKeywordsYes, #TODO Use some smarter hash-alike.
-    timestamp: $initTimestamp()
+    timestamp: initTimestamp().zulu
   )
 
 proc createDatabaseNotified*(config: ConfigZoom): DatabaseNotified =
   DatabaseNotified(
     keywordSignature: $config.patternKeywordsYes & $config.patternKeywordsYes, #TODO Use some smarter hash-alike.
-    timestamp: $initTimestamp()
+    timestamp: initTimestamp().zulu
   )
 
 proc deserialiseDatabaseNotified*(fields: seq[string]): DatabaseNotified =
@@ -27,4 +34,8 @@ proc deserialiseDatabaseNotified*(fields: seq[string]): DatabaseNotified =
   )
 
 proc deserialiseDatabaseNotifiedTimestamp*(fields: seq[string]): Timestamp =
-  fields[fields.find("timestamp").succ].parseZulu
+  try:
+    fields[fields.find("timestamp").succ].parseZulu
+  except:
+    logger.log(lvlError, "[model/database.deserialiseDatabaseNotifiedTimestamp] Unable to deserialise this Redis Query result:\p" & fields[fields.find("timestamp").succ])
+    raise getCurrentException()
