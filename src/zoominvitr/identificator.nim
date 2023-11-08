aa241ed6e46bbafa8a8108adcb79bef882767692
Akito <the@akito.ooo>
2023-11-08 02:30:21 +0100

    Re-Vamp Identification Hashing

--- src/zoominvitr/identificator.nim
+++ src/zoominvitr/identificator.nim
@@ -11,22 +11,43 @@ type NoKeywordsFoundDefect* = object of Defect
 template raiseNoKeywordsFoundDefect* =
   raise NoKeywordsFoundDefect.newException("No `patternKeywordsYes` and `patternKeywordsNo` found!")
 
-func genHash*(configs: varargs[seq[ConfigZoomPatternKeyword]]): int =
+func withPrefix(prefix: string, configs: seq[ConfigZoomPatternKeyword]): tuple[prefix: string, keywords: seq[ConfigZoomPatternKeyword]] =
+  (prefix, configs)
+
+func withPrefixYes(configs: seq[ConfigZoomPatternKeyword]): tuple[prefix: string, keywords: seq[ConfigZoomPatternKeyword]] =
+  withPrefix("y", configs)
+
+func withPrefixNo(configs: seq[ConfigZoomPatternKeyword]): tuple[prefix: string, keywords: seq[ConfigZoomPatternKeyword]] =
+  withPrefix("n", configs)
+
+func genHash(configs: varargs[tuple[prefix: string, keywords: seq[ConfigZoomPatternKeyword]]]): int =
   # Alphabetically sort keywords, so it does not generate a new hash,
   # simply by changing the keyword order.
   # Lower all characters, for the same reason.
-  @configs.concat.mapIt(it.keywords.mapIt(it.toLowerAscii).sorted.join).mapIt(it.toLowerAscii).sorted.join.hash
+  #
+  # Differentiates between YES and NO,
+  # so changing YES or NO keywords
+  # will make a difference in hashes,
+  # when different contexts are
+  # using different YES & NO constellations,
+  # while still using the same keywords themselves.
+  #
+  # Example:
+  #   Context 1: Yes: Hello, World No: I, Hate, You
+  #   Context 2: Yes: Hello No: World, I, Hate, You
+  #   Both contexts must be considered different.
+  @configs.mapIt(it.prefix & it.keywords.concat.mapIt(it.keywords.mapIt(it.toLowerAscii).sorted.join).mapIt(it.toLowerAscii).sorted.join).hash
 
 proc genHash*(yesOpt, noOpt: Option[seq[ConfigZoomPatternKeyword]]): int =
   let
     yesThere = yesOpt.isSome
     noThere = noOpt.isSome
   if yesThere and noThere:
-    genHash(yesOpt.get, noOpt.get)
+    genHash(yesOpt.get.withPrefixYes, noOpt.get.withPrefixNo)
   elif yesThere:
-    genHash(yesOpt.get)
+    genHash(yesOpt.get.withPrefixYes)
   elif noThere:
-    genHash(noOpt.get)
+    genHash(noOpt.get.withPrefixNo)
   else:
     raiseNoKeywordsFoundDefect()
 
